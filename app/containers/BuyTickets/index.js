eebd6f3fd96f0529c9f946acb7ce941d0f7667be
michaelgichia <gichuru.gichia@gmail.com>
2017-09-25 12:39:24 +0300

    Added mobile responsive for buying ticket page.

--- app/containers/BuyTickets/index.js
+++ app/containers/BuyTickets/index.js
@@ -6,22 +6,45 @@
 
 import React from 'react';
 import MenuTabsLargeScreen from 'components/MenuTabsLargeScreen';
-import { Button, Form } from 'semantic-ui-react';
+import { Button, Form, Menu } from 'semantic-ui-react';
 import '!!style-loader!css-loader!./buy-tickets.css';
 import productImage from './product-banner.jpg';
 
-export class BuyTickets extends React.PureComponent {
-  // eslint-disable-line react/prefer-stateless-function
+export class BuyTickets extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  state = { activeItem: 'home' }
+ 
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render() {
+    const { activeItem } = this.state;  
     return (
       <div>
-        <div className="buy-ticket-information lg-screen">
-          <span>FRIDAY 28 OCT 17:00-23:00 // 15 DAYS TO GO</span>
-          <span>BRAEBURN THEATER OFF GITANGA // GET DIRECTIONS</span>
+        <div className="mobile titcket-image-wrap grid-33">
+          <div>
+            <img src={productImage} alt="product" />
+          </div>
+
+          <div className="ticket-title">
+            <h4>TEDx LAVINGTON WOMEN: INDEPENDENT EVENT</h4>
+          </div>
         </div>
+
+        <div className="buy-ticket-information lg-screen">
+          <h5>FRIDAY 28 OCT 17:00-23:00 // 15 DAYS TO GO</h5>
+          <h5>BRAEBURN THEATER OFF GITANGA // <a href="">GET DIRECTIONS</a></h5>
+        </div>
+
+        <Menu className="mobile" pointing secondary fluid widths={3}>
+          <Menu.Item name="BUY TICKETS" active={activeItem === 'BUY TICKETS'} onClick={this.handleItemClick} />
+          <Menu.Item name="EVENT INFO" active={activeItem === 'EVENT INFO'} onClick={this.handleItemClick} />
+          <Menu.Item name="GALLERY" active={activeItem === 'GALLERY'} onClick={this.handleItemClick} />
+        </Menu>
+
         <MenuTabsLargeScreen />
-        <div className="description-wrap">
-          <div className="grid-33">
+
+        <div className="ticket-description-wrap">
+        
+          <div className="desktop grid-33">
             <div>
               <img src={productImage} alt="product" />
             </div>
@@ -29,6 +52,52 @@ export class BuyTickets extends React.PureComponent {
 
           <div className="grid-66 price-table">
             <table>
+              <tbody>
+                <tr>
+                  <th>TYPE</th>
+                  <th>PRICE</th>
+                  <th>QUANTITY</th>
+                </tr>
+                <tr>
+                  <td>EARLY BIRD</td>
+                  <td>KES. 800</td>
+                  <td><input type="number" defaultValue={0} min="0" /></td>
+                </tr>
+                <tr>
+                  <td>Sub-total</td>
+                  <td></td>
+                  <td>KES. 0.00</td>
+                </tr>
+                <tr>
+                  <td>EARLY BIRD EARLY BIRD</td>
+                  <td>KES. 800</td>
+                  <td><input type="number" defaultValue={0} min="0" /></td>
+                </tr>
+                <tr>
+                  <td>Sub-total</td>
+                  <td></td>
+                  <td>KES. 0.00</td>
+                </tr>
+                <tr>
+                  <td>EARLY BIRD EARLY BIRD</td>
+                  <td>KES. 800</td>
+                  <td><input type="number" defaultValue={0} min="0" /></td>
+                </tr>
+                <tr>
+                  <td>Sub-total</td>
+                  <td></td>
+                  <td>KES. 0.00</td>
+                </tr>
+                <tr>
+                  <td>TOTAL</td>
+                  <td />
+                  <td className="ticket-total">KES. 0.00</td>
+                </tr>
+              </tbody>
+            </table>
+
+
+            <table className="desktop">
               <tbody>
                 <tr>
                   <th>TYPE</th>
@@ -68,7 +137,8 @@ export class BuyTickets extends React.PureComponent {
                 </tr>
               </tbody>
             </table>
-            <Form>
+
+            <Form className="pay-tickets">
               <label>SEND TICKECTS TO:</label>
               <Form.Group widths={2}>
                 <Form.Input placeholder="name" />
@@ -79,13 +149,21 @@ export class BuyTickets extends React.PureComponent {
                 <Form.Input placeholder="cornfirm email" />
               </Form.Group>
             </Form>
-            <Form>
+            <Form className="pay-tickets">
               <label>PROMO CODE (optional)</label>
               <Form.Group widths={2}>
                 <Form.Input placeholder="promo code" />
                 <div className="payment-btn-wrap">
-                  <Button color="yellow" className="btn-payment">MOBILE PAYMENT</Button>
-                  <Button color="yellow" className="btn-payment">CARD PAYMENT</Button>
+                  <div>
+                    <Button className="btn-payment" fluid>
+                      MOBILE PAYMENT
+                    </Button>
+                  </div>
+                  <div>
+                    <Button className="btn-payment" fluid>
+                      CARD PAYMENT
+                    </Button>
+                  </div>
                 </div>
               </Form.Group>
             </Form>
@@ -97,4 +175,3 @@ export class BuyTickets extends React.PureComponent {
 }
 
 export default BuyTickets;
-
