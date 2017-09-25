d1d1a5f27c1351e7ca832a15336e0050baf4f887
michaelgichia <gichuru.gichia@gmail.com>
2017-09-25 17:40:04 +0300

    Page not found.

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -17,9 +17,11 @@ import messages from './messages';
 export default class NotFound extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div style={{height: "100vh", textAlign: "center"}}>
+        <h1>
+          <FormattedMessage {...messages.header} />
+        </h1>
+      </div>
     );
   }
 }
