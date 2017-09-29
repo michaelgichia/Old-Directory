375a17c61ae47d409e49e617f0910fddd59364ca
michaelgichia <gichuru.gichia@gmail.com>
2017-09-29 06:57:49 +0300

    Changed the landing navbar.

--- app/containers/DirectoryLandingPage/index.js
+++ app/containers/DirectoryLandingPage/index.js
@@ -7,11 +7,27 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import "!!style-loader!css-loader!./dlp-desktop.css";
+import SiteLogo from './site-logo.png';
 
 export class DirectoryLandingPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div>
+      <div className="search-main">
+        <div className="site-logo">
+          <img src={SiteLogo} alt=""/>          
+        </div>
+        <div className="search-wrap">
+          <div>
+            <input
+              className="search-input"
+              type="type"
+              placeholder="Search within category"
+              autoComplete="off"
+              role="textbox"
+              spellCheck="off"
+            />
+          </div>
+        </div>
       </div>
     );
   }
