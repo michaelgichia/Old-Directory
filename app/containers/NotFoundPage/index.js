093de6bff79446a136fb68de0d0c2d39f71b0925
michaelgichia <gichuru.gichia@gmail.com>
2017-09-30 10:05:38 +0300

    Fix footer and not found page.

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -10,22 +10,26 @@
  */
 
 import React from 'react';
+import mookhLogo from './logo-dark.png';
 import EventNavBar from 'containers/EventNavBar';
-import { FormattedMessage } from 'react-intl';
-
-import messages from './messages';
+import "!!style-loader!css-loader!./not-found-page.css";
 
 export default class NotFound extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div>
-        <EventNavBar />
-        <div style={{height: "75vh", textAlign: "center"}}>
-          <h1 style={{marginTop: 120}}>404</h1>
-          <h1 style={{marginTop: 30}}>
-            <FormattedMessage {...messages.header} />
-          </h1>
-        </div>
+      <div style={{minHeight: "90vh", position: "relative", color: "#343A41", backgroundColor: "#faea29"}}>
+        <nav className="footer-nav">
+          <ul>
+            <li><a href="/#"><img src={mookhLogo} alt="" /></a></li>
+            <li><a href="#">START SELLING</a></li>
+          </ul>
+        </nav>
+        <main className="not-found-main">
+          <div>
+            <h1>404</h1>
+            <h1>PAGE NOT FOUND</h1>
+          </div>
+        </main>
       </div>
     );
   }
