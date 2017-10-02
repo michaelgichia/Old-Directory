701ebe8beca1eb436377f4fd2d19c963db7d650d
michaelgichia <gichuru.gichia@gmail.com>
2017-10-02 20:21:39 +0300

    Add active class.

--- app/components/EventInfoMenu/index.js
+++ app/components/EventInfoMenu/index.js
@@ -4,23 +4,31 @@
 *
 */
 
-import React from 'react';
-import { Icon } from 'semantic-ui-react';
-import '!!style-loader!css-loader!./invent-info-menu.css';
+import React from "react";
+import { Icon } from "semantic-ui-react";
+import { getFormattedDate } from "utils/helperFunctions";
+import "!!style-loader!css-loader!./invent-info-menu.css";
 
-class EventInfoMenu extends React.Component { // eslint-disable-line react/prefer-stateless-function
+const getDirectionBaseAPI = "https://www.google.com/maps/search/?api=1&query=";
+
+class EventInfoMenu extends React.Component {
+  // eslint-disable-line react/prefer-stateless-function
   render() {
+    const { eventName, eventVenue } = this.props;
+    const location = eventVenue.toLocaleLowerCase().replace(/,/g, "+");
+
     return (
       <div className="event-information-wrap">
         <div className="product-title">
-          <h3>TEDx LAVINGTON WOMEN: INDEPENDENT EVENT</h3>
+          <h3>{eventName}</h3>
         </div>
         <div className="description header">
           <h5>
             <Icon name="clock" />FRIDAY 28 OCT 17:00-23:00 // 15 DAYS TO GO
           </h5>
-          <a href="#">
-            <Icon name="marker" />BRAEBURN THEATER OFF GITANGA
+          <a href={`${getDirectionBaseAPI}${location}`} target="_blank">
+            <Icon name="marker" />
+            {eventVenue}
           </a>
         </div>
       </div>
@@ -28,8 +36,6 @@ class EventInfoMenu extends React.Component { // eslint-disable-line react/prefe
   }
 }
 
-EventInfoMenu.propTypes = {
-
-};
+EventInfoMenu.propTypes = {};
 
 export default EventInfoMenu;
