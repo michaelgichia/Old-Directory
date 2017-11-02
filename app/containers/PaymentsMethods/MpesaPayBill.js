2a9a209c06c8b8dfb0fad8daf65c63d79562e60e
michaelgichia <gichuru.gichia@gmail.com>
2017-11-02 23:47:45 +0300

    Add all buttons to appropiate pages.

--- app/containers/PaymentsMethods/MpesaPayBill.js
+++ app/containers/PaymentsMethods/MpesaPayBill.js
@@ -1,36 +1,63 @@
+/*
+ *
+ * MpesaPayBill
+ *
+ */
+
 import React, { PureComponent } from "react";
+import TabsBottomWrap from "components/TabsBottomWrap";
+import TabsBodyWrap from "components/TabsBodyWrap";
+import { PaymentButtons } from "components/Buttons";
 import "!!style-loader!css-loader!./mpesa-paybill.css";
 
 export class MpesaPayBill extends PureComponent {
   render() {
     return (
       <div>
-        <p className="header">Follow these instructions to via PAYBILL</p>
-        <div className="mpesa-paybill-wrap">
+        <TabsBodyWrap>
+          <p className="header">Follow these instructions to via PAYBILL</p>
+          <div className="mpesa-paybill-wrap">
+            <div>
+              <p>Go to the M-Pesa menu</p>
+              <p>Select Lipa na M-Pesa</p>
+              <p>Select Paybill</p>
+              <span>
+                <p>Enter Business Number</p>
+                <p>570937</p>
+              </span>
+              <span>
+                <p>Enter Account Number</p>
+                <p>FKJRBIW4</p>
+              </span>
+              <span>
+                <p>Enter Amount</p>
+                <p>Ksh. 1750</p>
+              </span>
+              <p>Enter your M-Pesa pin</p>
+            </div>
+            <div>
+              <span>Please wait while your order is being</span>
+              <span>processed...</span>
+              <span>100% Done</span>
+            </div>
+          </div>
+        </TabsBodyWrap>
+        <TabsBottomWrap>
           <div>
-            <p>Go to the M-Pesa menu</p>
-            <p>Select Lipa na M-Pesa</p>
-            <p>Select Paybill</p>
-            <span>
-              <p>Enter Business Number</p>
-              <p>570937</p>
-            </span>
-            <span>
-              <p>Enter Account Number</p>
-              <p>FKJRBIW4</p>
-            </span>
-            <span>
-              <p>Enter Amount</p>
-              <p>Ksh. 1750</p>
-            </span>
-            <p>Enter your M-Pesa pin</p>
+            <PaymentButtons
+              id="store"
+              bsKlass="secondary shadow"
+              label="PREVIOUS"
+            />
           </div>
           <div>
-            <span>Please wait while your order is being</span>
-            <span>processed...</span>
-            <span>100% Done</span>
+            <PaymentButtons
+              id="nextOne"
+              bsKlass="primary shadow"
+              label="CONTINUE"
+            />
           </div>
-        </div>
+        </TabsBottomWrap>
       </div>
     );
   }
--- app/containers/PaymentsMethods/MpesaPush.js
+++ app/containers/PaymentsMethods/MpesaPush.js
@@ -1,26 +1,51 @@
+/*
+ *
+ * MpesaPush
+ *
+ */
+
 import React, { PureComponent } from "react";
 import MpesaPushImage from "./MpesaPushImage.png";
+import TabsBottomWrap from "components/TabsBottomWrap";
+import TabsBodyWrap from "components/TabsBodyWrap";
+import { PaymentButtons } from "components/Buttons";
+
 import "!!style-loader!css-loader!./mpesa-push.css";
 
 export class MpesaPush extends PureComponent {
   render() {
     return (
-      <div className="mpesa-push-wrap">
-        <div>
-          <p>Follow these instructions to complete transaction</p>
-          <p>Check your phone and follow the instructions on your screen.</p>
-          <p>
-            If nothing appears in the next 10 seconds,<a
-              href="#"
-              onClick={this.props.onClick}
-            >
-              click here
-            </a>
-          </p>
-        </div>
-        <div>
-          <img src={MpesaPushImage} alt="" />
-        </div>
+      <div>
+        <TabsBodyWrap>
+          <div className="mpesa-push-wrap">
+            <div>
+              <p>Follow these instructions to complete transaction</p>
+              <p>
+                Check your phone and follow the instructions on your screen.
+              </p>
+              <p>
+                If nothing appears in the next 10 seconds,<a
+                  href="#"
+                  onClick={this.props.onClick}
+                >
+                  click here
+                </a>
+              </p>
+            </div>
+            <div>
+              <img src={MpesaPushImage} alt="" />
+            </div>
+          </div>
+        </TabsBodyWrap>
+        <TabsBodyWrap>
+          <div>
+            <PaymentButtons
+              id="store"
+              bsKlass="secondary shadow"
+              label="PREVIOUS"
+            />
+          </div>
+        </TabsBodyWrap>
       </div>
     );
   }
