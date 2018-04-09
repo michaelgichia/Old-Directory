79367c3a7f093df5f5e68f8f980b4b2ec0491774
michaelgichia <gichuru.gichia@gmail.com>
2018-04-09 11:43:44 +0300

    Phone number validation.

--- app/containers/EventBuyTicket/EventInput.js
+++ app/containers/EventBuyTicket/EventInput.js
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 const EventInput = styled(Input)`
   @media (min-width: 768px) {
     height: 36px;
-    font-size: 16px !important;
   }
 
   @media (max-width: 768px) {
